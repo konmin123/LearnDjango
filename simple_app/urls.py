d7048da96d3a39c87f1fd71e704d2b2f37241072
konmin <MindlinKS@yandex.ru>
2022-05-01 22:35:10 +0300

    add block add djangorestframework

--- simple_app/urls.py
+++ simple_app/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('datetime', views.SimpleView.as_view())
+    path('datetime/', views.SimpleView.as_view()),
+    path('blog/', views.SimpleView1.as_view()),
 ]
--- simple_app/views.py
+++ simple_app/views.py
@@ -8,3 +8,16 @@ class SimpleView(View):
         html = f"{datetime.now()}"
 
         return HttpResponse(html)
+
+
+class SimpleView1(View):
+    def get(self, request:HttpRequest):
+        html = f"Hi"
+
+        return HttpResponse(html)
+
+    def post(self, request: HttpRequest):
+        ...
+
+    def put(self, request: HttpRequest):
+        ...
